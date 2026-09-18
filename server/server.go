feb9f328da15fd306865f5d7429846c57fe4a882
abel malu <abellmalu@gmail.com>
2026-09-18 11:27:48 +0300

    feat:add graceful shutdown

--- server/server.go
+++ server/server.go
@@ -1,29 +1,72 @@
 package server
 
 import (
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/abelmalu/fluxts/config"
 	"github.com/abelmalu/fluxts/platform"
 	"go.uber.org/zap"
-
-	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
 )
 
 type Server struct {
-	router *gin.Engine
-	logger *platform.Logger
-	cfg    *config.Config
+	grpcServer *grpc.Server
+	logger     *platform.Logger
+	cfg        *config.Config
 }
 
-func NewServer(router *gin.Engine, logger *platform.Logger, cfg *config.Config) *Server {
+func NewServer(logger *platform.Logger, cfg *config.Config) *Server {
+
+	grpcServer := grpc.NewServer()
+
 	return &Server{
-		router: router,
-		logger: logger,
-		cfg:    cfg,
+		logger:     logger,
+		grpcServer: grpcServer,
+		cfg:        cfg,
 	}
 }
 
 func (s *Server) StartServer() error {
 
-	s.logger.Info("Starting server on port:", zap.String("port", s.cfg.GRPCPORT))
-	return s.router.Run()
+	lis, err := net.Listen("tcp", s.cfg.GRPCPORT)
+	if err != nil {
+		s.logger.Error("failed to listen tcp requests", zap.Error(err))
+		return err
+	}
+
+	serverErrors := make(chan error, 1)
+	go func() {
+
+		s.logger.Info("Starting server on port:", zap.String("port", s.cfg.GRPCPORT))
+
+		if err := s.grpcServer.Serve(lis); err != nil && err != grpc.ErrServerStopped {
+			s.logger.Error("failed to serve", zap.Error(err))
+
+			serverErrors <- err
+
+		}
+
+	}()
+
+	shutdownSignal := make(chan os.Signal, 1)
+	signal.Notify(shutdownSignal, syscall.SIGINT, syscall.SIGTERM)
+
+	select {
+
+	case err := <-serverErrors:
+		return err
+	case sig := <- shutdownSignal:
+ 
+		s.logger.Info("Shutdown signal received, stopping server...", zap.String("signal", sig.String()))
+		
+		s.grpcServer.GracefulStop()
+		return nil
+
+		
+	}
+
 }
