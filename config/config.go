a91a127add737df807a7d2c734e7e40b01cfe21a
abel malu <abellmalu@gmail.com>
2026-09-25 14:49:34 +0300

    feat:add multi instance producer

--- config/config.go
+++ config/config.go
@@ -2,52 +2,23 @@ package config
 
 import (
 	"errors"
-	"fmt"
 	"os"
 )
 
 type Config struct {
-	DBURL           string
-	GRPCPORT        string
-	KafkaBrokersURL []string
-	RedisADD        string
-	MinioADD        string
+	GRPCPORT string
 }
 
 func LoadConfig() (*Config, error) {
 
 	cfg := Config{}
 
-
 	cfg.GRPCPORT = ":" + os.Getenv("GRPC_PORT")
 
 	if cfg.GRPCPORT == "" {
 
-		return nil, errors.New("GRPC_POR environment variable is required!")
+		return nil, errors.New("GRPC_PORT environment variable is required!")
 
-	}
-
-	cfg.DBURL = os.Getenv("DB_URL")
-
-	if cfg.DBURL == "" {
-
-		return nil, errors.New("DB_URL environment variable is required!")
-	}
-
-	cfg.KafkaBrokersURL = []string{os.Getenv("KAFKA_BROKERS_URL")}
-	if len(cfg.KafkaBrokersURL) == 0 {
-		return nil, fmt.Errorf("KAFKA_BROEKER_URL is required")
-	}
-
-	cfg.RedisADD = os.Getenv("REDIS_URL")
-	if cfg.RedisADD == "" {
-		return nil, fmt.Errorf("REDIS_ADD is required")
-	}
-
-
-	cfg.MinioADD = os.Getenv("MINIO_URL")
-	if cfg.MinioADD == "" {
-		return nil, fmt.Errorf("MINIO_ADD is required")
 	}
 
 	return &cfg, nil
---
+++ producer/config.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+)
+
+type Config struct {
+	Producers        int
+	SamplesPerSecond int
+	BatchSize        int
+	FlushInterval    time.Duration
+	Duration         time.Duration
+	MaxInflight      int
+	ServerAddr		 string
+}
+
+func ParseFlags() (Config, error) {
+	cfg := Config{}
+	flag.StringVar(&cfg.ServerAddr, "addr", "localhost:50051", "FluxTS gRPC server address (host:port)")
+	flag.IntVar(&cfg.Producers, "producers", 1, "number of concurrent producer streams")
+	flag.IntVar(&cfg.SamplesPerSecond, "sps", 10, "samples per second, per simulated series")
+	flag.IntVar(&cfg.BatchSize, "batch-size", 100, "max samples per batch before a forced flush")
+	flag.DurationVar(&cfg.FlushInterval, "flush-interval", 500*time.Millisecond, "flush at least this often")
+	flag.DurationVar(&cfg.Duration, "duration", 0, "total run time (0 = until Ctrl-C)")
+	flag.IntVar(&cfg.MaxInflight, "max-inflight", 1, "max unacknowledged batches per stream")
+	flag.Parse()
+
+	if cfg.ServerAddr == "" {
+		return cfg, fmt.Errorf("-addr is required")
+	}
+	if cfg.Producers <= 0 {
+		return cfg, fmt.Errorf("-producers must be > 0")
+	}
+	if cfg.SamplesPerSecond <= 0 {
+		return cfg, fmt.Errorf("-sps must be > 0")
+	}
+	if cfg.BatchSize <= 0 {
+		return cfg, fmt.Errorf("-batch-size must be > 0")
+	}
+	if cfg.FlushInterval <= 0 {
+		return cfg, fmt.Errorf("-flush-interval must be > 0")
+	}
+	if cfg.MaxInflight <= 0 {
+		return cfg, fmt.Errorf("-max-inflight must be > 0")
+	}
+	return cfg, nil
+}
